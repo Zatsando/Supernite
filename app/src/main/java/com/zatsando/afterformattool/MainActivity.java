54a74f3329e04e0e373758dc712dd8b1587411cb
Zatsando <zvracy13@gmail.com>
2021-01-04 17:16:32 +0200

    changes

--- app/src/main/java/com/zatsando/afterformattool/MainActivity.java
+++ app/src/main/java/com/zatsando/afterformattool/MainActivity.java
@@ -1,13 +1,18 @@
 package com.zatsando.afterformattool;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AlertDialogLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox intellij_check;
     private CheckBox vsc_check;
     private CheckBox eclipse_check;
+    private CheckBox steam_check;
+    private CheckBox geforcenow_check;
+    private CheckBox epic_check;
+    private CheckBox torrent_check;
+    private CheckBox vlc_check;
+    private CheckBox anydesk_check;
+    private Context context;
+    ArrayList<String> commands = new ArrayList<>();
+    String chocoInstaller = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));";
+    String textCommand = commands.toString().replace(",", "").replace("[" , "").replace("]", "");
+    String finalCommandFile = chocoInstaller + textCommand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +46,32 @@ public class MainActivity extends AppCompatActivity {
         intellij_check = findViewById(R.id.intellij_check);
         vsc_check = findViewById(R.id.vsc_check);
         eclipse_check = findViewById(R.id.eclipse_check);
+        steam_check = findViewById(R.id.steam_check);
+        geforcenow_check = findViewById(R.id.geforcenow_check);
+        epic_check = findViewById(R.id.epic_check);
+        torrent_check = findViewById(R.id.torrent_check);
+        vlc_check = findViewById(R.id.vlc_check);
+        anydesk_check = findViewById(R.id.anydesk_check);
+
     }
 
-    ArrayList<String> commands = new ArrayList<>();
-
-    //read and clear buttons
+    //read button to send
     public void ready_button(View view) {
-        String chocoInstaller = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));";
-        String textCommand = commands.toString().replace(",", "").replace("[" , "").replace("]", "");
 
-        //intend method to create the text file
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, chocoInstaller+textCommand);
-        sendIntent.setType("text/plain");
-        Intent shareIntent = Intent.createChooser(sendIntent, null);
-        startActivity(shareIntent);
-
-        Toast.makeText(getApplicationContext(), "You are indeed, ready!", Toast.LENGTH_SHORT).show();
+        if (commands.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "You have to check something, you know...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, finalCommandFile);
+                sendIntent.setType("plain/text");
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+        }
     }
 
+    //clear button
     public void clear_button(View view) {
         commands.clear();
         if (discord_check.isChecked()) {
@@ -73,16 +95,35 @@ public class MainActivity extends AppCompatActivity {
         if (eclipse_check.isChecked()) {
             eclipse_check.setChecked(false);
         }
+        if (steam_check.isChecked()) {
+            steam_check.setChecked(false);
+        }
+        if (geforcenow_check.isChecked()) {
+            geforcenow_check.setChecked(false);
+        }
+        if (epic_check.isChecked()) {
+            epic_check.setChecked(false);
+        }
+        if (torrent_check.isChecked()){
+            torrent_check.setChecked(false);
+        }
+        if (vlc_check.isChecked()){
+            vlc_check.setChecked(false);
+        }
+        if (anydesk_check.isChecked()){
+            anydesk_check.setChecked(false);
+        }
 
         Toast.makeText(getApplicationContext(), "Cleared!", Toast.LENGTH_SHORT).show();
     }
 
-    //app buttons functionality
+    //check buttons functionality
     public void brave_check(View view) {
         if (brave_check.isChecked()) {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install brave -a;");
         } else {
+            commands.remove("choco install brave -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -92,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install firefox -a;");
         } else {
+            commands.remove("choco install firefox -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -101,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install discord -a;");
         } else {
+            commands.remove("choco install discord -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -110,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install telegram -a;");
         } else {
+            commands.remove("choco install telegram -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -119,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install intellij -a;");
         } else {
+            commands.remove("choco install intellij -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -128,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
             commands.add("choco install vsc -a;");
         } else {
+            commands.remove("choco install vsc -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -135,9 +181,71 @@ public class MainActivity extends AppCompatActivity {
     public void eclipse_check(View view) {
         if (eclipse_check.isChecked()) {
             Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
-            commands.add("choco install vsc -a;");
+            commands.add("choco install eclipse -a;");
         } else {
+            commands.remove("choco install eclipse -a;");
             Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
         }
     }
+
+    public void steam_check(View view) {
+        if (steam_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install steam -a;");
+        } else {
+            commands.remove("choco install steam -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void geforcenow_check(View view) {
+        if (geforcenow_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install nvidia-geforce-now -a;");
+        } else {
+            commands.remove("choco install nvidia-geforce-now -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void epic_check(View view) {
+        if (epic_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install epicgameslauncher -a;");
+        } else {
+            commands.remove("choco install epicgameslauncher -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void torrent_check(View view) {
+        if (torrent_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install qbittorrent -a;");
+        } else {
+            commands.remove("choco install qbittorrent -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void vlc_check(View view) {
+        if (vlc_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install vlc -a;");
+        } else {
+            commands.remove("choco install vlc -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void anydesk_check(View view) {
+        if (anydesk_check.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Checked!", Toast.LENGTH_SHORT).show();
+            commands.add("choco install anydesk -a;");
+        } else {
+            commands.remove("choco install anydesk -a;");
+            Toast.makeText(getApplicationContext(), "Unchecked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
