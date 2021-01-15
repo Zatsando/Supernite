05844a9c7323288f0b82ed640b80305904b911c2
Zatsando <zvracy13@gmail.com>
2021-01-15 20:24:01 +0200

    changes

--- app/src/main/java/com/zatsando/afterformattool/MainActivity.java
+++ app/src/main/java/com/zatsando/afterformattool/MainActivity.java
@@ -11,9 +11,13 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -70,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
                 fos.write(finalCommandFile.getBytes());
+
                 Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
                         Toast.LENGTH_LONG).show();
             } catch (FileNotFoundException e) {
@@ -78,13 +83,15 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-           //share the goddamn file!!!
-            Intent sendIntent = new Intent();
-            sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, FILE_NAME);
-            sendIntent.setType("plain/text");
-            Intent shareIntent = Intent.createChooser(sendIntent, null);
-            startActivity(shareIntent);
+            File scriptFile = getFileStreamPath("data/user/0/com.zatsando.afterformattool/files/scriptinstaller.ps1");
+
+//            share the goddamn file!!!
+//            Intent sendIntent = new Intent();
+//            sendIntent.setAction(Intent.ACTION_SEND);
+//            sendIntent.putExtra(Intent.EXTRA_TEXT, scriptFile);
+//            sendIntent.setType("plain/text");
+//            Intent shareIntent = Intent.createChooser(sendIntent, "scriptinstaller.ps1");
+//            startActivity(shareIntent);
         }
     }
 
